c61095784489193b180a119b95fc53ad1ec81766
prasadanadimpalli-sureify <prasadababu@sureify.com>
2025-01-11 12:50:00 +0530

    First code

---
+++ src/First.java
@@ -0,0 +1,32 @@
+package src;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.apache.commons.io.FileUtils;
+
+public class First {
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        System.setProperty("webdriver.chrome.driver", "./chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://google.com");
+
+        TakesScreenshot screnshot = (TakesScreenshot) driver;
+
+        File file = screnshot.getScreenshotAs(OutputType.FILE);
+
+        FileUtils.copyFile(file, new File("./Screenshots/Screen.png"));
+        // Thread.sleep(5000);
+        driver.findElement(By.xpath("//a[@aria-label='Sign in']")).click();;
+        // driver.close();
+
+    }
+}
